5c40719fce7fde6f4ed7e68f3749bc46a9eced70
Josh Duplisea <duplisea.josh@gmail.com>
2021-12-08 10:26:35 -0400

    modified config

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Josh Duplisea',
+    site_url: 'https://portfolio-five-pi-17.vercel.app/posts',
+      feed_url: 'https://portfolio-five-pi-17.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
